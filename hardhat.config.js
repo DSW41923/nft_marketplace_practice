56cebafc1def39390e1ecd112bf5735520a3c26f
DSW41923 <dsw41923@gmail.com>
2022-01-15 23:06:47 +0800

    Set up project base

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+const fs = require('fs')
+//const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
